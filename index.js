0acfb7d6e7027204da93ba2dea5c0906f7b6f3ef
Mike Coletta <coletta.mike@gmail.com>
2021-02-20 20:35:34 -0800

    Created basic prompt and init functions

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const promptUser = () => 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project called?'
+        }
+    ]);
+
+const init = () => {
+    promptUser();
+}
+
+init();
